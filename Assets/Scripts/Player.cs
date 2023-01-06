cac12d315dbb75bc87572fe0b0c3b237709f0bc2
Kayque 2 -  Win 11 <kayquelopes@outlook.com.br>
2023-01-06 16:24:23 -0300

    Player Prefab and Animations Fixed!

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float Speed;
-    public float RotSpeed;   
+    public float RotSpeed;
     public float Gravity;
 
     private float Rotation;
@@ -15,39 +15,94 @@ public class Player : MonoBehaviour
     CharacterController controller;
     Animator anim;
 
+    bool isReady;
+
 
     void Start()
     {
         controller = GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();
     }
 
-    
+
     void Update()
     {
         Move();
+        GetMouseInput();
     }
 
     void Move()
     {
+
         if (controller.isGrounded)
+            Debug.Log("no chão");
         {
             if (Input.GetKey(KeyCode.W))
             {
-                MoveDirection = Vector3.forward * Speed;
-                MoveDirection = transform.TransformDirection(MoveDirection);
+                if (!anim.GetBool("isAttacking"))
+                {
+                    anim.SetBool("isWalking", true);
+                    anim.SetInteger("transition", 1);
+                    MoveDirection = Vector3.forward * Speed;
+                    MoveDirection = transform.TransformDirection(MoveDirection);
+
+                }
+                else
+                {
+                    anim.SetBool("isWalking", false);
+                    MoveDirection = Vector3.zero;
+                    StartCoroutine(Attack(1));
+                }
             }
 
             if (Input.GetKeyUp(KeyCode.W))
             {
+                anim.SetBool("isWalking", false);
+                anim.SetInteger("transition", 0);
                 MoveDirection = Vector3.zero;
             }
         }
 
-        Rotation += Input.GetAxis("Horizontal") * RotSpeed * Time.deltaTime;
+        Rotation += Input.GetAxisRaw("Horizontal") * RotSpeed * Time.deltaTime;
         transform.eulerAngles = new Vector3(0, Rotation, 0);
 
         MoveDirection.y -= Gravity * Time.deltaTime;
         controller.Move(MoveDirection * Time.deltaTime);
+    }
+
+    void GetMouseInput()
+    {
+        if (controller.isGrounded)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (anim.GetBool("isWalking"))
+                {
+                    anim.SetBool("isWalking", false);
+                    anim.SetInteger("transition", 0);
+                }
+
+                if (!anim.GetBool("isWalking"))
+                {
+                    StartCoroutine(Attack(0));
+                }
+
+            }
+        }
+    }
+
+    IEnumerator Attack(int transitionValue)
+    {
+        if (!isReady)
+        {
+            isReady = true;
+            anim.SetBool("isAttacking", true);
+            anim.SetInteger("transition", 2);
+            yield return new WaitForSeconds(1.3f);
+            anim.SetBool("isAttacking", false);
+            anim.SetInteger("transition", transitionValue);
+            isReady = false;
+        }
     }
 
 }
