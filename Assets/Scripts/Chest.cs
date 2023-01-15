b0abee1462582f669375fdcf89269787133191ca
Kayque 2 -  Win 11 <kayquelopes@outlook.com.br>
2023-01-14 22:20:51 -0300

    Add Chest Script and Animations!

---
+++ Assets/Scripts/Chest.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chest : MonoBehaviour
+{
+    private Animator anim;
+    public float ColliderRadius;
+    public bool isOpened = false;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+        GetPlayer();
+    }
+
+    void GetPlayer()
+    {
+        foreach (Collider c in Physics.OverlapSphere((transform.position + transform.forward * ColliderRadius), ColliderRadius))
+        {
+            if (c.gameObject.CompareTag("Player"))
+            {
+                if (Input.GetMouseButtonDown(0))
+                {
+                    OpenChest();
+                }
+            }
+        }
+    }
+
+    void OpenChest()
+    {
+        anim.SetTrigger("open");
+        isOpened = true;
+    }
+}
